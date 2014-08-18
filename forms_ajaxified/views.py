6803fdbe2ba177a905f5cdfd1f8a60750677eee8
Martin Brochhaus <mbrochh@gmail.com>
2014-08-18 02:01:33 +0200

    Turned AjaxFormView into AjaxFormViewMixin

--- forms_ajaxified/views.py
+++ forms_ajaxified/views.py
@@ -2,10 +2,9 @@
 import json
 
 from django.http import HttpResponse
-from django.views.generic import FormView
 
 
-class AjaxFormView(FormView):
+class AjaxFormViewMixin(object):
     """Turns a FormView into a special view that can handle AJAX requests."""
     def form_invalid(self, form):
         if self.request.is_ajax():
@@ -15,14 +14,14 @@ class AjaxFormView(FormView):
                     'trigger_element': self.request.REQUEST['trigger_element'],
                     'errors': form.errors,
                 }), mimetype='application/json')
-        return super(AjaxFormView, self).form_valid(form)
+        return super(AjaxFormViewMixin, self).form_valid(form)
 
     def form_valid(self, form):
-        form.save()
+        self.object = form.save()
         if self.request.is_ajax():
             return HttpResponse(
                 json.dumps({
                     'success': 1,
                     'trigger_element': self.request.REQUEST['trigger_element'],
                 }), mimetype='application/json')
-        return super(AjaxFormView, self).form_valid(form)
+        return super(AjaxFormViewMixin, self).form_valid(form)
